5aa77c262da75952f5a7825f2f628186297052ab
jegs01 <bodcafe@gmail.com>
2025-08-11 15:19:05 +0100

    Added tracking view

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function RideHailingPage() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <PusherClient user={user} />
+      <PusherClient user={user} setPage={setPage} />
       <Header user={user} setUser={setUser} setPage={setPage} />
 
       <main className="container mx-auto px-4 py-8">
